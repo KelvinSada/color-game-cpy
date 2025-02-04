5705521004c4447377187e4256a9313f05817b28
Novoh .K. Sada <kelvinsada23@gmail.com>
2025-02-04 15:33:23 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,94 @@
+import React, {useState} from "react";
+import "./styles.css";
+
+function App() {
+const sixRandomColors = [];
+const [userPass,setUserPass] = useState();
+const [wins,setwins] = useState(0);
+const [displayMessage,setDisplayMessage] = useState("");
+const [restart,setRestart] = useState(false)
+const [shake, setShake] = useState(false);
+
+function getSixColors(){
+  for (var i=0;i<6;i++){
+    sixRandomColors[i] = [Math.floor((Math.random()*256)),Math.floor((Math.random()*256)),Math.floor((Math.random()*256))]
+  }
+}
+userPass!==3&&getSixColors()
+
+let showColor = sixRandomColors[Math.floor(Math.random()*6)]
+const displayColor =`rgb(${showColor[0]},${showColor[1]},${showColor[2]})`
+const showColorIndex = sixRandomColors.indexOf(showColor)
+
+
+function getIndexValue(id){
+
+  const showColorIndex = sixRandomColors.indexOf(showColor)
+  if (id === showColorIndex){
+    setUserPass(true)
+    setTimeout(() => setUserPass(), 500);
+    setwins(prev=>{
+      return prev+1;
+    })
+    setDisplayMessage("Correct pick 🏆")
+    setTimeout(() => setDisplayMessage(""), 500);
+    setShake(true);
+    
+  } else if (id !== showColorIndex){
+    setUserPass(false)
+    getSixColors()
+    setwins(prev=>{
+      return 0;
+    })
+    setDisplayMessage("Wrong, You Lost 😢")
+    setTimeout(() => setDisplayMessage(""), 500);
+    setShake(false);
+
+  }
+
+  setTimeout(() => setShake(false), 500);
+
+}
+
+  function restartAction(){
+    setRestart(true)
+    setwins(0)
+    setUserPass(false)
+    setDisplayMessage("")
+
+  }
+
+  return (
+    <main>
+      <header data-testid="gameInstructions" className="header">GUESS THE <span className="color-title">COLOR</span></header>
+      
+      <div className="scoreSection">
+        <div class="scoree">Score: <button className="score" data-testid="score">{wins}</button></div>
+        <button onClick={restartAction} data-testid="newGameButton" >Reset</button>
+      </div>
+      
+      <div>
+        <div style={{backgroundColor:displayColor}} className="colorBox" data-testid="colorBox">
+        </div>
+      </div>
+      
+      <div className={shake ? "shake" : "loss"} data-testid="gameStatus">
+      <p className="status">{displayMessage}</p>
+      </div>
+      
+      <div className="colorOptionsContainer">
+      {sixRandomColors.map((x,index)=>(
+      <div onClick={()=>getIndexValue(index)}
+      key={index}
+      id={index}
+      style={{backgroundColor:`rgb(${x[0]},${x[1]},${x[2]})`}} 
+      className="colorPicks" 
+      data-testid="colorOption">
+      </div>
+      ))}
+      </div>
+
+    </main>
+  );
+}
+export default App;
